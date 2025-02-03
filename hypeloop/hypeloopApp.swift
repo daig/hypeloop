d10635c3f3a1be6e35e1313f1ef24b69dfa144a6
Dai <david.girardo@gauntletai.com>
2025-02-03 15:34:30 -0600

    Initial Commit

---
+++ hypeloop/hypeloopApp.swift
@@ -0,0 +1,17 @@
+//
+//  hypeloopApp.swift
+//  hypeloop
+//
+//  Created by David Girardo on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct hypeloopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
